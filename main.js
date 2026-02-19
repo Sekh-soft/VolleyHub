b1977b6b8941bf9938d112002836a617301705be
Sekh-soft <60543676+Sekh-soft@users.noreply.github.com>
2026-02-19 06:37:48 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
 /* ===============================
-   VOLLEYWOLLEY GOD MODE JS
+   VOLLEYHUB GOD MODE JS
 ================================ */
 
-/*
-=================================
-Mouse Glow Effect
-=================================
-Moves glowing background based
-on mouse position.
-*/
+/* ===============================
+   Mouse Glow Effect
+================================ */
 
-// Select glow element
 const glow = document.querySelector(".mouse-glow");
 
-// Run only if element exists (prevents console errors)
+// Prevent errors if element not found
 if (glow) {
 
     document.addEventListener("mousemove", (e) => {
 
-        // Update glow position
         glow.style.left = e.clientX + "px";
         glow.style.top = e.clientY + "px";
 
